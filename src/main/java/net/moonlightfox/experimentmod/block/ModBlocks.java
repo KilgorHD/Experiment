dcef5ef7294a233c69ea01de83ac9bcf424719d8
KilgoHD <nicolaspuck@gmail.com>
2024-01-29 20:51:29 +0100

    Initial commit

---
+++ src/main/java/net/moonlightfox/experimentmod/block/ModBlocks.java
@@ -0,0 +1,12 @@
+package net.moonlightfox.experimentmod.block;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.moonlightfox.experimentmod.ExperimentMod;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS=
+            DeferredRegister.create(ForgeRegistries.BLOCKS, ExperimentMod.MOD_ID);
+
+}
---
+++ src/main/java/net/moonlightfox/experimentmod/item/ModItems.java
@@ -0,0 +1,16 @@
+package net.moonlightfox.experimentmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.moonlightfox.experimentmod.ExperimentMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS=
+            DeferredRegister.create(ForgeRegistries.ITEMS, ExperimentMod.MOD_ID);
+
+
+
+
+
+}
